ad588611ef642b3bc6b83e18bfe72ac1c9ef98db
macroaa <44141486+macroaa@users.noreply.github.com>
2022-05-10 17:38:40 +0800

    Add files via upload

---
+++ AR/server.js
@@ -0,0 +1,19 @@
+
+import path from 'path';
+const _dirname = path.resolve();
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set("/", "html");
+app.use(express.static(path.join(_dirname, "/")));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res)=>{
+    res.render('index');
+});
+
+app.listen(8080, ()=> {
+    console.log("Listening on http://localhost:8080");
+});
